3a7726fa414f6447b3b77d6b5c3e53e49a8b23e4
proartpro <42385971+proartpro@users.noreply.github.com>
2018-10-01 09:50:00 +0300

    Add files via upload

---
+++ Homework 2.1/src/Homework_2_1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Homework_2_1 {
+
+//Homework
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ievadi vardu un uzvardu: ");
+		String input = scanner.nextLine();
+
+		input = input.trim();
+		int index = input.indexOf(" ");
+		System.out.println("index: " + index);
+
+		String firstname = input;
+		if (index > 0) {
+		 firstname = input.substring(0, index);
+		}
+		
+		firstname = 
+				firstname.substring(0, 1).toUpperCase() + 
+				firstname.substring(1).toLowerCase();
+		System.out.println("'" + firstname + "'");
+
+		if (index > 0) {
+			String lastname = input.substring(index).trim().toUpperCase();
+			System.out.println("'" + lastname + "'");
+		} else {
+			System.out.println();
+		}
+
+		scanner.close();
+
+	}
+
+}
